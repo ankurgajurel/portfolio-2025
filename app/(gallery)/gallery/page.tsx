4b4dc4a0abec0603335cd95ebcb9174d3b09636e
Ankur Gajurel <ankurgajurel02@gmail.com>
2025-05-12 05:15:03 +0545

    fix: add revalidation for gallery api hit

--- app/(gallery)/gallery/page.tsx
+++ app/(gallery)/gallery/page.tsx
@@ -11,7 +11,12 @@ export interface Media {
 export default async function Gallery() {
   if (!process.env.GALLERY_JSON_ENDPOINT) redirect("/");
 
-  const res = await fetch(process.env.GALLERY_JSON_ENDPOINT);
+  const res = await fetch(process.env.GALLERY_JSON_ENDPOINT, {
+    next: {
+      revalidate: 60,
+    },
+  });
+
   const media: Media[] = await res.json();
 
   return (
